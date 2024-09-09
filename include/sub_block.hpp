dc03890e183d3978a57a17bbf0f8f242c51f3548
ProjectRobal <patryk1-70@wp.pl>
2024-09-09 02:05:27 +0200

    I have added save and load options

--- include/sub_block.hpp
+++ include/sub_block.hpp
@@ -25,23 +25,16 @@ namespace snn
     class SubBlock
     {   
 
-        std::shared_ptr<Mutation> mutate;
-
         std::mt19937 gen; 
 
-        std::normal_distribution<number> distribution;
         std::normal_distribution<number> global;
 
         std::uniform_real_distribution<float> uniform;
 
-        number weight;
-        number best_weight;
-
-        long double reward;
-        long double best_reward;
-
         SIMDVector pop_weights;
         SIMDVector pop_rewards;
+
+        SIMDVector best_weights;
 
         number max_std;
         number min_std;
@@ -67,13 +60,6 @@ namespace snn
 
             this->gen = std::mt19937(rd());
 
-            this->weight = 0;
-            this->reward = 0;
-            this->best_reward = -9999999.f;
-            this->best_weight = 0.f;
-
-            // this->Ticks = 0;
-
             this->choosen_weight = 0;
             
             this->uniform = std::uniform_real_distribution<float>(0.f,1.f);
@@ -83,19 +69,16 @@ namespace snn
         SubBlock(std::shared_ptr<Mutation> _mutate)
         :SubBlock()
         {
-            this->mutate = _mutate;
+
         }
 
         void setup(size_t inputSize,std::shared_ptr<Initializer> init)
         {
             number std = std::sqrt(2.f/inputSize);
-            this->distribution = std::normal_distribution<number>(0.f,std);   
             this->global = std::normal_distribution<number>(0.f,std);  
 
             this->max_std = std;
             this->min_std = MIN_STD;
-
-            this->weight = this->distribution(this->gen);
 
             for(size_t i=0;i<Populus;++i)
             {
@@ -103,33 +86,6 @@ namespace snn
                 this->pop_rewards.append(0.f);
             }
 
-        }
-
-        number get_max()
-        {
-            number max = 0;
-            for(size_t i=1;i<this->pop_weights.size();++i)
-            {
-                if( pop_rewards[i] > pop_rewards[max] )
-                {
-                    max = i;
-                }
-            }
-
-            return this->pop_weights.pop(max);
-        }
-
-        void estimate_specie(const SIMDVector& weights)
-        {
-            number mean = weights.reduce()/weights.size();
-
-            SIMDVector w = weights - mean;
-
-            w = w*w;
-
-            number std = std::sqrt(w.reduce()/w.size());
-
-            this->distribution = std::normal_distribution<number>(mean,std);
         }
 
         void chooseWorkers()
@@ -150,58 +106,11 @@ namespace snn
                 std::cout<<norm_rewards<<std::endl;
             }
 
-            snn::SIMDVector best;
-
-            for(size_t i=0;i<5;++i)
-            {
-                best.append(this->pop_weights[this->pop_weights.size()-i-1]);
-            }
-
-            // if( this->Ticks >= 200 )
-            // {
-
-            //     if( this->id == 10 )
-            //     {
-            //         std::cout<<"Crossover!"<<std::endl;
-            //     }
-
-            //     // number reward_mean = this->pop_rewards.reduce() / this->pop_rewards.size();
-
-            //     this->pop_rewards /=10.f;
-
-            //     // // this->pop_rewards -= this->pop_rewards[this->pop_rewards.size()-1];
-
-            //     // number best_mean = best.reduce()/best.size();
-
-            //     // best = best - best_mean;
-
-            //     // best = best * best;
-
-            //     // std::normal_distribution<number> best_dist(best_mean,std::sqrt(best.reduce()/best.size()));
-
-            //     // for(size_t i=0;i<Populus;++i)
-            //     // {
-            //     //     this->pop_weights.set(best_dist(this->gen),i);
-            //     //     this->pop_rewards.set(0,i);
-            //     // }
-
-            //     // this->choosen_weight = 0;
-
-            //     this->Ticks = 0;
-
-            //     return;
-            // }
-
-            // this->Ticks++;
-
-
             // perform crossover!
             
-
-                // a k best weights leave alone
-                // other weights will be discarded and replaced by mutated best weight with variance calculated from best weight or just use simple mutation :)
-                // rest of population will be random weights
-
+            // a k best weights leave alone
+            // other weights will be discarded and replaced by mutated best weight with variance calculated from best weight or just use simple mutation :)
+            // rest of population will be random weights
 
             for(size_t i=0;i<Populus/2;++i)
             {
@@ -212,18 +121,34 @@ namespace snn
 
                     // number weight = best[i] + best_dist(this->gen);
                     this->pop_weights.set(this->global(this->gen),i);
+                    
 
                 }
             }
 
-            for(size_t i=Populus/2;i<Populus;++i)
+            for(size_t i=Populus/2;i<Populus-1;++i)
             {
 
                 if( this->uniform(this->gen) <= 0.25f )
                 {
-                    number d_weight = ( best - this->pop_weights[i] ).reduce() / 5;
+                    // number d_weight = ( best - this->pop_weights[i] ).reduce() / 5;
 
-                    number weight = this->pop_weights[i] + d_weight * ( this->uniform(this->gen) + 0.1f );
+                    // number weight = this->pop_weights[i] + d_weight * ( this->uniform(this->gen) + 0.1f );
+
+                    std::normal_distribution<number> dist(0.0,this->max_std/10);
+
+                    number mean;
+
+                    if(this->best_weights.size()>0)
+                    {
+                        mean = this->best_weights.reduce() / (this->best_weights.size()+1);
+                    }
+                    else
+                    {
+                        mean = this->pop_weights[this->pop_weights.size()-1];
+                    }
+
+                    number weight = mean + dist(this->gen)*(this->uniform(this->gen)<0.5);
 
                     this->pop_weights.set(weight,i);
 
@@ -259,8 +184,24 @@ namespace snn
         // let's tread reward not as reward rather than error
         void giveReward(long double reward)
         {   
-
+            
             this->pop_rewards.set(0.4f*this->pop_rewards[this->choosen_weight]+reward,this->choosen_weight);
+           
+            if(reward>=0)
+            {
+                if((this->best_weights.size() == 0)||(this->best_weights[this->best_weights.size()-1]!=this->pop_weights[this->choosen_weight]))
+                {
+                    this->best_weights.append(this->pop_weights[this->choosen_weight]);
+
+                    if(this->best_weights.size()>10)
+                    {
+                        number b_w = this->best_weights.reduce()/this->best_weights.size();
+
+                        this->best_weights.clear();
+                        this->best_weights.append(b_w);
+                    }
+                }
+            }
         }
 
         number get()
@@ -283,13 +224,36 @@ namespace snn
 
             char buffer[SERIALIZED_NUMBER_SIZE];
 
-            serialize_number(this->distribution.mean(),buffer);
 
-            out.write(buffer,SERIALIZED_NUMBER_SIZE);
+            for(size_t i=0;i<Populus;++i)
+            {
+                serialize_number(this->pop_weights[i],buffer);
 
-            serialize_number(this->distribution.stddev(),buffer);
+                out.write(buffer,SERIALIZED_NUMBER_SIZE);
+            }
 
-            out.write(buffer,SERIALIZED_NUMBER_SIZE);
+            for(size_t i=0;i<Populus;++i)
+            {
+                serialize_number(this->pop_rewards[i],buffer);
+
+                out.write(buffer,SERIALIZED_NUMBER_SIZE);
+            }
+
+            // save rewards:
+            size_t reward_count = this->best_weights.size();
+
+            char count_buffer[sizeof(size_t)];
+
+            memcpy(count_buffer,(char*)&reward_count,sizeof(size_t));
+
+            out.write(count_buffer,sizeof(size_t));
+
+            for(size_t i=0;i<reward_count;++i)
+            {
+                serialize_number(this->best_weights[i],buffer);
+
+                out.write(buffer,SERIALIZED_NUMBER_SIZE);
+            }
 
         }
 
@@ -297,15 +261,44 @@ namespace snn
         {
             char buffer[SERIALIZED_NUMBER_SIZE];
 
-            in.read(buffer,SERIALIZED_NUMBER_SIZE);
+            for(size_t i=0;i<Populus;++i)
+            {
 
-            number mean = deserialize_number<number>(buffer);
+                in.read(buffer,SERIALIZED_NUMBER_SIZE);
 
-            in.read(buffer,SERIALIZED_NUMBER_SIZE);
+                number weight = deserialize_number<number>(buffer);
 
-            number std = deserialize_number<number>(buffer);
+                this->pop_weights.set(weight,i);
 
-            this->distribution = std::normal_distribution<number>(mean,std);
+            }
+
+            for(size_t i=0;i<Populus;++i)
+            {
+
+                in.read(buffer,SERIALIZED_NUMBER_SIZE);
+
+                number reward = deserialize_number<number>(buffer);
+
+                this->pop_rewards.set(reward,i);
+            
+            }
+
+            char count_buffer[sizeof(size_t)];
+
+            in.read(count_buffer,sizeof(size_t));
+
+            size_t rewards_count;
+
+            memcpy((char*)&rewards_count,count_buffer,sizeof(size_t));
+
+            for(size_t i=0;i<rewards_count;++i)
+            {
+                in.read(buffer,SERIALIZED_NUMBER_SIZE);
+
+                number weight = deserialize_number<number>(buffer);
+
+                this->best_weights.append(weight);
+            }
 
         }
 
