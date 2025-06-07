713d61361c91381c558f75bd97da29d7c3bb1c93
ProjectRobal <patryk1-70@wp.pl>
2025-06-08 00:09:32 +0200

    It may really works I hope so

--- include/evo_kan_block.hpp
+++ include/evo_kan_block.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <cstdint>
 #include <vector>
+#include <cmath>
 
 #include <simd_vector_lite.hpp>
 
@@ -31,7 +32,7 @@ namespace snn
             {
                 this->x0 = x;
                 this->y0 = y;
-                this->b = 100.f; // default stretch value
+                this->b = 1000.f; // default stretch value
             }
 
             SplineNode()
@@ -76,9 +77,69 @@ namespace snn
 
         struct Spline
         {
+            private:
+
             std::vector<SplineNode> nodes;
 
             number biggest_y;
+
+            inline void fit_add_new_node(number x,number output,number target)
+            {
+                number dy = target - output;
+
+                SplineNode new_node(x,dy);
+
+                this->nodes.push_back(new_node);
+            }
+
+            inline void fit_move_towards_y(number x,number output,number target)
+            {
+                for( SplineNode& node : nodes )
+                {
+                    number error = abs( x - node.x0 );
+
+                    if( error <= 0.00001f )
+                    {
+                        number dy = target - output;
+
+                        node.y0 = ( node.y0 + dy ) / 2.f;
+
+                    }
+                }
+
+            }
+
+            inline void fit_stretch_node(number x,number output,number target)
+            {
+
+                number min_x_dist = abs( this->nodes[0].x0 - x );
+                size_t min_i = 0;
+
+                for( size_t i = 1; i < this->nodes.size() ; i++ )
+                {
+                    number x_dist = abs( this->nodes[i].x0 - x );
+
+                    if( x_dist < min_x_dist )
+                    {
+                        min_i = i;
+                        min_x_dist = x_dist;
+                    }
+                }
+
+                SplineNode& node = this->nodes[min_i];
+
+                if( target >= node.y0 )
+                {
+
+                    this->fit_add_new_node(x,output,target);
+
+                    return;
+                }
+
+                node.b = std::log(target/node.y0)/( -( x - node.x0 )*( x - node.x0 ) );
+            }
+
+            public:
 
             Spline()
             {
@@ -87,14 +148,34 @@ namespace snn
 
             void fit(number x,number output,number target)
             {
+                number error = abs(output-target);
+
+                // add a node when there is no node
+                if( this->nodes.size() == 0 )
+                {
+                    this->fit_add_new_node(x,output,target);
+
+                    return;
+                }
+
+                // when error is not so big find nearest node and nudge it b value to fit target
+                if( error <= 0.2f )
+                {
+                    this->fit_stretch_node(x,output,target);
+
+                    return;
+                }
+                // when error is very close to zero, find node very close to target and move it's y towards target
+                else if( error < 0.00001f )
+                {   
+                    this->fit_move_towards_y(x,output,target);
+
+                    return;
+                }
 
                 // add new node when error is too high
 
-                number dy = target - output;
-
-                SplineNode new_node(x,dy);
-
-                this->nodes.push_back(new_node);
+                this->fit_add_new_node(x,output,target);
 
             }
 
